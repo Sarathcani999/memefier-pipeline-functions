508bc8a5189407a2dc82e409e638517a1104e245
Sarath C Ani <mailsforsarath98@gmail.com>
2020-06-25 14:08:02 +0530

    Triggers for follower count and following count completed

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,52 @@
 const functions = require('firebase-functions');
-// const db = require('')
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const increment = admin.firestore.FieldValue.increment(1);
+const decrement = admin.firestore.FieldValue.increment(-1);
 
 exports.createFollower = functions.firestore
-    .document('followers/{document}')
+    .document('followers/{id}')
     .onCreate((snap, context) => {
-      console.log(snap.data());
+        console.log(snap.data());
 
-      return 0;
+        const followed_by = snap.data().followed_by;
+        const following = snap.data().following;
+        const promises = [];
+
+        const p1 = admin.firestore().collection('userDetails').doc(followed_by).update({
+            following : increment
+        });
+        promises.push(p1);
+
+        const p2 = admin.firestore().collection('userDetails').doc(following).update({
+            followers : increment
+        });
+        promises.push(p2);
+
+        return Promise.all(promises)
+
+    });
+
+exports.deleteFollower = functions.firestore
+    .document('followers/{id}')
+    .onDelete((snap, context) => {
+        console.log(snap.data());
+
+        const followed_by = snap.data().followed_by;
+        const following = snap.data().following;
+
+        const promises = [];
+
+        const p1 = admin.firestore().collection('userDetails').doc(followed_by).update({
+            following : decrement
+        });
+        promises.push(p1);
+
+        const p2 = admin.firestore().collection('userDetails').doc(following).update({
+            followers : decrement
+        });
+        promises.push(p2);
+
+        return Promise.all(promises)
     });
