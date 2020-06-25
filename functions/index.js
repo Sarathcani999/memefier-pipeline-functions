e145608b721fd35d724c5fa9090d6342f0027582
Sarath C Ani <mailsforsarath98@gmail.com>
2020-06-25 12:46:10 +0530

    new changes

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+// const db = require('')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createFollower = functions.firestore
+    .document('followers/{document}')
+    .onCreate((snap, context) => {
+      console.log(snap.data());
+
+      return 0;
+    });
